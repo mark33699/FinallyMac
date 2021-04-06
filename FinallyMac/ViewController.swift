b0c08071db5fbe422f732252ce382a278c950c16
angelmark <mark.hsieh@angelsctek.com>
2021-04-06 12:22:53 +0800

    NSSwitch

--- FinallyMac/ViewController.swift
+++ FinallyMac/ViewController.swift
@@ -8,19 +8,16 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var label: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("Hello Mac")
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    @IBAction func toggle(_ sender: NSSwitch) {
+        label.stringValue = sender.state == .on ? "Hello Mac" : "Goodbye Mac"
     }
-
-
+    
 }
 
